bec558d5e1b8598a273fe403f48560faebbca3cf
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-01-22 22:24:21 +0200

    commit

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const http = require('node:http');
+
+const {sdf} = require('./some_dir/helper');
+sdf();
+
 console.log('Hello world');
 console.log(__dirname);
 console.log(__filename);
 console.log(process.cwd());
+
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n2555');
+})
+server.listen(3000);
---
+++ some_dir/helper.js
@@ -0,0 +1,7 @@
+const foo = () => {
+    console.log('hello from helper');
+}
+
+console.log('helper !!!!!!!!!');
+
+module.exports = {sdf: foo};
