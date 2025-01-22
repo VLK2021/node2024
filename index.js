9391ab9e1fff4a6d152f52bdda3d4b56cf0b60e1
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-01-22 22:51:02 +0200

    commit

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-console.log('Hello world');
-console.log(__dirname);
-console.log(__filename);
-console.log(process.cwd());
+const http = require('node:http');
+
+//HTTP
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World\n2222');
+// })
+// server.listen(3000);
+
+//Path
