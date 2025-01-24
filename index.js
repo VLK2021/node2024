cd92ecd070b39094788fc3e017e8394776299f61
vova <volodymyrkostiuk2021@gmail.com>
2025-01-25 00:39:04 +0200

    commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const http = require('node:http');
 const path = require('node:path');
 const rl = require('node:readline/promises');
+const fscl = require('node:fs');
+const fs = require('node:fs/promises');
+const os = require('node:os');
+// const {emitter} = require('./emiter');
+
 
 
 const foo = async () => {
@@ -21,12 +26,54 @@ const foo = async () => {
 // console.log(path.isAbsolute('./home/maksym/WORK/Lessons/jan-2024-nodejs/index.js'));
 
 // Readline
-    const rlInstance = rl.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    const name = await rlInstance.question('Enter your name: ');
-    console.log(`Hello, ${name}!`);
+//     const rlInstance = rl.createInterface({
+//         input: process.stdin,
+//         output: process.stdout
+//     });
+//     const name = await rlInstance.question('Enter your name: ');
+//     console.log(`Hello, ${name}!`);
+
+    // FS
+    // const pathToFile = path.join(__dirname, 'some_dir', 'test.txt');
+    // await fs.writeFile(pathToFile, 'Hello, world!')
+    // const data = await fs.readFile(pathToFile, 'utf-8')
+    // console.log(data);
+    // await fs.appendFile(pathToFile, '\n Hello, world again!')
+    //
+    // await fs.mkdir(path.join(__dirname, 'some_dir', 'new_dir', 'new_dir2'), {recursive: true})
+    // await fs.rmdir(path.join(__dirname, 'some_dir', 'new_dir', 'new_dir2'))
+
+    // await fs.writeFile(path.join(__dirname, 'some_dir', 'new_dir', 'new_dir2', 'test.txt'), 'Hello, world!')
+    // await fs.rm(path.join(__dirname, 'some_dir', 'new_dir'), {recursive: true})
+    // await fs.unlink(path.join(__dirname, 'test.txt'))
+    // await fs.rename(path.join(__dirname, 'some_dir', 'test.txt'), path.join(__dirname, 'test222.txt'))
+    // await fs.copyFile(path.join(__dirname, 'some_dir', 'test222.txt'), path.join(__dirname, '111.txt'))
+    // const stat = await fs.stat(path.join(__dirname, 'some_dir', 'test222.txt'))
+    // console.log(stat.isDirectory())
+    // console.log(stat.isFile())
+    //
+    // // FS stream
+    // const readStream = fscl.createReadStream(path.join(__dirname, 'big_file.pdf'))
+    // const writeStream = fscl.createWriteStream(path.join(__dirname, 'big_file_copy.pdf'))
+    //
+    // readStream.on('data', (chunk) => {
+    //     console.log(chunk);
+    //     writeStream.write(chunk)
+    // });
+    // readStream.pipe(writeStream)
+
+    // OS
+    // console.log(os.arch());
+    // console.log(os.cpus());
+    // console.log(os.hostname());
+    // console.log(os.homedir());
+    // console.log(os.freemem() / 1024 / 1024 / 1024);
+    // console.log(os.totalmem() / 1024 / 1024 / 1024);
+
+    // emitter.emit('test2', 'qwe', 'asd', 222)
+    // emitter.emit('test2', 'SSS', 'WWWWW', 111)
+    // emitter.emit('create_user', {name: 'maks', age: 28})
+    // emitter.emit('create_user', {name: 'Dima', age: 28})
 
 
 }
