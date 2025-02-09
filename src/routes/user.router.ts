194f14dd3739459d912955e0f1829a647fd34c55
vlk2022 <volodymyrkostiuk2021@gmail.com>
2025-02-09 21:42:43 +0200

    l5

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,14 +1,27 @@
 import { Router } from "express";
 
 import { userController } from "../controlers/user.controller";
+import { commonMiddleware } from "../middlewares/common.middleware";
 
 const router = Router();
 
 router.get("/", userController.getList);
 router.post("/", userController.create);
 
-router.get("/:userId", userController.getById);
-router.delete("/:userId", userController.delete);
-router.put("/:userId", userController.update);
+router.get(
+  "/:userId",
+  commonMiddleware.isIdValid("userId"),
+  userController.getById,
+);
+router.delete(
+  "/:userId",
+  commonMiddleware.isIdValid("userId"),
+  userController.delete,
+);
+router.put(
+  "/:userId",
+  commonMiddleware.isIdValid("userId"),
+  userController.update,
+);
 
 export const userRouter = router;
